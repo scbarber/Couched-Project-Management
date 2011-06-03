8a8633083d0917c30bf511d4764e83b0532b7aec
Seth Croston Barber <seth@0kelvin.net>
2011-06-03 16:40:28 -0700

    Added view of prospective clients.

--- evently/active-projects/_changes/data.js
+++ evently/active-projects/_changes/data.js
@@ -2,12 +2,11 @@ function(data) {
   // $.log(data)
   var project;
   var today = Date.today();
-  var projects =  {
+  return {
     projects : data.rows.map(function(r) {
         project = r.value;
         project.css_class = (today > Date.parse(project.due_date)) ? "overdue" : "";
         return project;
     })
   };
-  return projects;
 };
