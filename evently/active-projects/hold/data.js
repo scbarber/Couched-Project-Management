de1333879f182c87581803c617dd137b857fe876
Seth Croston Barber <seth@0kelvin.net>
2011-06-06 13:51:16 -0700

    Renaming "active-projects" to "projects".

--- evently/active-projects/hold/data.js
+++
@@ -1,8 +0,0 @@
-function(data) {
-  // $.log(data)
-  return {
-    projects : data.rows.map(function(r) {
-        return r.value;
-    })
-  };
-};
--- evently/active-projects/prospect/selectors/a.edit/click.js
+++
@@ -1,5 +0,0 @@
-function(){
-    var project_id = $(this).parents('tr').eq(0).attr('id');
-    $(this).trigger('edit', project_id);
-    return(false);
-}
