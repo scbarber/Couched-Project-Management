ce701178f81765b1433dce81a47e64cb98e68712
Seth Croston Barber <seth@0kelvin.net>
2011-06-06 13:49:27 -0700

    Renaming "active-projects" to "projects".

---
+++ evently/projects/active/selectors/tbody/_changes/data.js
@@ -0,0 +1,12 @@
+function(data) {
+  // $.log(data)
+  var project;
+  var today = Date.today();
+  return {
+    projects : data.rows.map(function(r) {
+        project = r.value;
+        project.css_class = (today > Date.parse(project.due_date)) ? "overdue" : "";
+        return project;
+    })
+  };
+};
